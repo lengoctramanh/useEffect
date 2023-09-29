7aaa892d03af5b6bc8810b1a77105bce78231410
lengoctramanh <letramanh2182005@gmail.com>
2023-09-29 08:40:27 +0700

    Progress-Bar

--- src/components/UpButton.jsx
+++ src/components/UpButton.jsx
@@ -1,17 +1,24 @@
 import React, { Fragment, useEffect, useState } from 'react';
 
 const UpButton = () => {
+  // Tạo state để lưu trữ vị trí ban đầu của trang web là false
   const [topButton, setTopButton] = useState(false);
+  /**Khi người dùng kéo trang web, hãy cập nhật state để phản ánh
+   * vị trí hiện tại của trang web */
   const handleTopButton = () => {
-    if (window.scrollY > 20) {
+    if (window.scrollY > 100) {
       setTopButton(true);
     } else {
       setTopButton(false);
     }
   };
+  /*Khi trang web được tải, hãy lưu trữ vị trí ban,
+    đầu của trang web vào state*/
   useEffect(() => {
     window.addEventListener('scroll', handleTopButton);
   }, []);
+  /**Khi người dùng click vào button, hãy đặt lại vị trí của
+   * trang web về vị trí ban đầu */
   const top = () => {
     window.scrollTo(0, 0);
   };
@@ -29,3 +36,11 @@ const UpButton = () => {
 };
 
 export default UpButton;
+/** B1:xét điều kiện của topButton
+ * B2:Nếu điều kiện topButton có giá trị true, thì React sẽ
+ * đánh giá biểu thức ( <button className="top-btn" onClick={top}>
+ * <span>^</span> </button> ).
+  B3:Nếu điều kiện topButton có giá trị false,
+  thì React sẽ trả về null.
+  Biểu thức này dùng để xét một nút quay lại đầu trang
+ */
